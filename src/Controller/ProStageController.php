902922ac247a7033d12989dceb9c31f8dc08cac0
Pako Mathieu <pkmt@pkmt.home>
2018-12-10 13:38:25 +0100

    github de merde

---
+++ src/Controller/ProStageController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProStageController extends AbstractController
+{
+    /**
+     * @Route("/", name="pro_stage")
+     */
+    public function index()
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    /**
+     * @Route("/entreprise/", name="pro_stage_entreprise")
+     */
+    public function afficherPageEntreprise()
+    {
+      return $this->render('pro_stage/entreprises.html.twig');
+    }
+
+    /**
+     * @Route("/formations/", name="pro_stage_formation")
+     */
+    public function afficherPageFormation()
+    {
+      return $this->render('pro_stage/formations.html.twig');
+    }
+
+    /**
+     * @Route("/stages/{id}", name="pro_stage_stages")
+     */
+    public function afficherPageStage($id)
+    {
+      return$this->render('pro_stage/stages.html.twig',['idStage'=>$id]);
+    }
+}
