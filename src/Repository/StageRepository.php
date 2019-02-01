6262d96d339c1deca2e2e789232de9a7187e30bc
Pako Mathieu <pkmt@Pkmt.local>
2019-02-01 15:48:51 +0100

    bug

--- src/Repository/StageRepository.php
+++ src/Repository/StageRepository.php
@@ -19,22 +19,21 @@ class StageRepository extends ServiceEntityRepository
         parent::__construct($registry, Stage::class);
     }
 
-    // /**
-    //  * @return Stage[] Returns an array of Stage objects
-    //  */
-    /*
-    public function findByExampleField($value)
+     /**
+     * @return Stage[] Returns an array of Stage objects
+      */
+
+    public function findByExampleField($nomEntreprise)
     {
         return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
+            ->join('s.entreprise','e')
+            ->andWhere('e.intitule = :nom')
+            ->setParameter('nom', $nomEntreprise)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+
 
     /*
     public function findOneBySomeField($value): ?Stage
