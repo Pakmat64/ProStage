6b1dd38333bd21bef2591ed633507ba760604c92
Pako Mathieu <pkmt@Pkmt.local>
2019-02-07 10:57:54 +0100

    Mise en place d'un formulaire de saisie d'un stage et d'une entreprise, de modification et soumission à la BD

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -9,6 +9,11 @@ use App\Entity\Entreprise;
 use App\Entity\Formation;
 use App\Repository\StageRepository;
 use App\Repository\EntrepriseRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\HttpFoundation\Request;
+	use Doctrine\Common\Persistence\ObjectManager;
+
 
 class ProStageController extends AbstractController
 {
@@ -87,7 +92,126 @@ class ProStageController extends AbstractController
     }
 
 
+    /**
+     * @Route("/entreprise/ajouter", name="pro_stage_entreprise_ajouter")
+     */
+    public function ajouterEntreprise(Request $requete, ObjectManager $manager)
+    {
+      $entreprise = new Entreprise();
 
+      //Construction du formulaire
+      $formulaireEntreprise = $this->createFormBuilder($entreprise)
+      ->add('intitule')
+      ->add('adresse',TextareaType::class)
+      ->add('activite')
+      ->add('URL',UrlType::class)
+      ->getForm();
+
+       $formulaireEntreprise->handleRequest($requete);
+
+       if ($formulaireEntreprise->isSubmitted() )
+	         {
+	            // Enregistrer la ressource en base de données
+	            $manager->persist($entreprise);
+	            $manager->flush();
+
+	            // Rediriger l'utilisateur vers la page d'accueil
+	            return $this->redirectToRoute('pro_stage');
+	         }
+
+      return $this->render('pro_stage/ajouterModifierEntreprise.html.twig',['vueFormulaire'=>$formulaireEntreprise->createView(),'action'=>"ajouter"]);
+    }
+
+    /**
+     * @Route("/entreprise/modifier/{id}", name="pro_stage_entreprise_modifier")
+     */
+    public function ModifierEntreprise(Request $requete, ObjectManager $manager,Entreprise $entreprise)
+    {
+
+      //Construction du formulaire
+      $formulaireEntreprise = $this->createFormBuilder($entreprise)
+      ->add('intitule')
+      ->add('adresse',TextareaType::class)
+      ->add('activite')
+      ->add('URL',UrlType::class)
+      ->getForm();
+
+       $formulaireEntreprise->handleRequest($requete);
+
+       if ($formulaireEntreprise->isSubmitted() )
+	         {
+	            // Enregistrer la ressource en base de données
+	            $manager->persist($entreprise);
+	            $manager->flush();
+
+	            // Rediriger l'utilisateur vers la page d'accueil
+	            return $this->redirectToRoute('pro_stage');
+	         }
+
+      return $this->render('pro_stage/ajouterModifierEntreprise.html.twig',['vueFormulaire'=>$formulaireEntreprise->createView(),'action'=>"modifier"]);
+    }
+
+    /**
+     * @Route("/stage/ajouter", name="pro_stage_stage_ajouter")
+     */
+    public function ajouterStage(Request $requete, ObjectManager $manager)
+    {
+      $stage = new Stage();
+
+      //Construction du formulaire
+      $formulaireStage = $this->createFormBuilder($stage)
+      ->add('initule')
+      ->add('descriptif',TextareaType::class)
+      ->add('domaine')
+      ->add('email')
+      ->add('URL',UrlType::class)
+      ->getForm();
+
+       $formulaireStage->handleRequest($requete);
+
+       if ($formulaireStage->isSubmitted() )
+	         {
+	            // Enregistrer la ressource en base de données
+	            $manager->persist($stage);
+	            $manager->flush();
+
+	            // Rediriger l'utilisateur vers la page d'accueil
+	            return $this->redirectToRoute('pro_stage');
+	         }
+
+      return $this->render('pro_stage/ajouterModifierStage.html.twig',['vueFormulaire'=>$formulaireStage->createView(),'action'=>"ajouter"]);
+    }
+
+    /**
+     * @Route("/stage/modifier/{id}", name="pro_stage_stage_modifer")
+     */
+    public function modifierStage(Request $requete, ObjectManager $manager,Stage $stage)
+    {
+
+
+      //Construction du formulaire
+      $formulaireStage = $this->createFormBuilder($stage)
+      ->add('initule')
+      ->add('descriptif',TextareaType::class)
+      ->add('domaine')
+      ->add('email')
+      ->add('URL',UrlType::class)
+      ->getForm();
+
+       $formulaireStage->handleRequest($requete);
+
+       if ($formulaireStage->isSubmitted() )
+	         {
+	            // Enregistrer la ressource en base de données
+	            $manager->persist($stage);
+	            $manager->flush();
+
+	            // Rediriger l'utilisateur vers la page d'accueil
+	            return $this->redirectToRoute('pro_stage');
+	         }
+
+      return $this->render('pro_stage/ajouterModifierStage.html.twig',['vueFormulaire'=>$formulaireStage->createView(),'action'=>"modifer"]);
+    }
 
 
 }
